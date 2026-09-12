10419a3964f85e82d91e2c0e67aaf41d41ecea37
ajpetre <ajpetr07@gmail.com>
2026-09-12 14:38:00 -0400

    Working Battle

--- aAIlga/obj/Debug/net8.0/aAIlga.AssemblyInfo.cs
+++ aAIlga/obj/Debug/net8.0/aAIlga.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aAIlga")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01841469886a5899d7a0038f25ba02d9e0a0f0de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a8ebcd0f0e888bf42e7d265ecc104faab55645d")]
 [assembly: System.Reflection.AssemblyProductAttribute("aAIlga")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aAIlga")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
